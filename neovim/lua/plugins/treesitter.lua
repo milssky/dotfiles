ff2ab7c346ea04dfd5eb408a7f682a7321e97e19
milssky <2furius@gmail.com>
2026-01-16 17:23:19 +0300

    fix indents

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
       require('nvim-treesitter.configs').setup {
         ensure_installed = { 'python', 'lua', 'vim', 'vimdoc' },
         highlight = { enable = true, additional_vim_regex_highlighting = false},
-        indent = { enable = true },
+        indent = { enable = false },
       }
     end
   }
